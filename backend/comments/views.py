0d9087ff64ac72e88cd62e65c08e06eba83767c3
Victoria Probst <victoria.probst13@gmail.com>
2022-06-24 12:19:09 -0400

    Established Updating Capabilities of Comments

--- backend/comments/views.py
+++ backend/comments/views.py
@@ -1,8 +1,11 @@
+
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import CommentsSerializer
 from .models import Comments
 from rest_framework import status
+
 
 # Create your views here.
 @api_view(['GET', 'POST'])
@@ -17,12 +20,14 @@ def comments_list(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-@api_view(['GET'])
-def comment_detail(requst, pk):
-    try:
-        comment = Comments.objects.get(pk=pk)
-        serializer = CommentsSerializer(comment)
-        
+@api_view(['GET', 'PUT'])
+def comment_detail(request, pk):
+    comment = get_object_or_404(Comments, pk=pk)
+    if request.method == 'GET':
+        serializer = CommentsSerializer(comment);
         return Response(serializer.data)
-    except Comments.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+    elif request.method == 'PUT':
+        serializer = CommentsSerializer(comment, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
