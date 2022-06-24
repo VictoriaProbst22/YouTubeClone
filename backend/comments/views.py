3cd5171ff63f8d4f26f322fa0791ac416f31d841
Victoria Probst <victoria.probst13@gmail.com>
2022-06-24 14:35:15 -0400

    Updated Postman Framework to Match User Stories and Requirements plus imported Permissions Classes and Authentication

--- backend/comments/views.py
+++ backend/comments/views.py
@@ -1,37 +1,34 @@
-
-import re
-from django.shortcuts import get_object_or_404
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from .serializers import CommentsSerializer
-from .models import Comments
 from rest_framework import status
-
+from rest_framework.response import Response
+from rest_framework.permission import IsAuthenticated, AllowAny
+from rest_framework.decorators import api_view, permission_classes
+from .models import Comments
+from .serializers import CommentsSerializer
 
 # Create your views here.
-@api_view(['GET', 'POST'])
-def comments_list(request):
-    if request.method == 'GET':
-        comments = Comments.objects.all()
-        serializer = CommentsSerializer(comments, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    elif request.method == 'POST':
-        serializer = CommentsSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def comment_detail(request, pk):
-    comment = get_object_or_404(Comments, pk=pk)
-    if request.method == 'GET':
-        serializer = CommentsSerializer(comment);
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    elif request.method == 'PUT':
-        serializer = CommentsSerializer(comment, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    elif request.method == 'DELETE':
-        comment.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def get_all_comments(request):
+    comment = Comments.objects.all()
+    serializer = CommentsSerializer(comment, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
+def user_comments(request):
+    print(
+        'User ', f"{request.user.id} {request.user.email} {request.user.username}")
+    if request.method == 'POST':
+        serializer = CommentsSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'GET':
+        comment = Comments.objects.filter(user_id=request.user.id)
+        serializer = CommentsSerializer(comment, many=True)
+        return Response(serializer.data)
+
+
