65fde0acbb370aac370df88387f73811625d061e
Victoria Probst <victoria.probst13@gmail.com>
2022-06-24 11:21:01 -0400

    Created Comments Serializer Class and File

---
+++ backend/comments/serializers.py
@@ -0,0 +1,8 @@
+from pyexpat import model
+from rest_framework import serializers
+from .models import Comments
+
+class CommentsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comments
+        fields = ['id', 'user', 'video_id', 'text', 'likes', 'dislikes']
