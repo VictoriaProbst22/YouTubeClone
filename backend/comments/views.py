f0f926e769c00d6bb8aeb96cdde13460a23fdc6a
Victoria Probst <victoria.probst13@gmail.com>
2022-06-24 11:50:36 -0400

    Established Post Requests in Postman

--- backend/comments/views.py
+++ backend/comments/views.py
@@ -2,11 +2,19 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import CommentsSerializer
 from .models import Comments
+from rest_framework import status
 
 # Create your views here.
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def comments_list(request):
-    comments = Comments.objects.all()
-    serializer = CommentsSerializer(comments, many=True)
-
-    return Response(serializer.data)
+    if request.method == 'GET':
+        comments = Comments.objects.all()
+        serializer = CommentsSerializer(comments, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = CommentsSerializer(data=request.data)
+        if serializer.is_valid() == True:
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors)
